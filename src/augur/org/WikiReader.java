d8a9d76e41ac33b0eef2e38e1b14a770faab31c4
Aniruddha Shastri <a.shastri@ufl.edu>
2014-10-02 15:24:43 -0400

    Added findMovieByYear method

--- src/augur/org/WikiReader.java
+++ src/augur/org/WikiReader.java
@@ -4,6 +4,8 @@
 package augur.org;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.wikipedia.Wiki;
 
@@ -15,12 +17,31 @@ public class WikiReader {
 
 	/**
 	 * @param args
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
+		//Wiki reader = new Wiki();
+		//String pageTitle = "Augur";
+		// System.out.println(reader.getPageText(pageTitle));
+		findMoviesByYear(2012);
+	}
+
+	/**
+	 * Finds all the movies that released in the year specified
+	 * 
+	 * @param year
+	 * @return The list of wikipedia movie URLs
+	 * @throws IOException 
+	 */
+	public static List<String> findMoviesByYear(int year) throws IOException {
+		List<String> movieUrlList = new ArrayList<String>();
 		Wiki reader = new Wiki();
-		String pageTitle = "Augur";
-		System.out.println(reader.getPageText(pageTitle));
+		String pageTitle = year + " in film";
+		String pageText = reader.getPageText(pageTitle);
+		
+		System.out.println(pageText);
+		
+		return movieUrlList;
 	}
 
 }
